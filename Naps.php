c477232eb3266f226c68d53a80e5fa261d41d834
Napasiokalifani <napskalifah42@gmail.com>
2024-09-20 18:25:44 +0300

    Update Naps.php

--- Naps.php
+++ Naps.php
@@ -1,10 +1,10 @@
 <?php 
 $Name=readline ("Insert your name please: ");
 $DOB=readline ("Insert your date of birth: ");
-$homeaddress =readline ("Insert your home address: ");
+$home_address =readline ("Insert your home address: ");
 $birthyear=(int)readline("Insert your birth year: ");
-$currentyear=(int)readline("Insert your  current year: ");
-$age=$currentyear-$birthyear;
+$current_year=(int)readline("Insert your  current year: ");
+$age=$current_year-$birthyear;
 print"Your most welcome home {$Name} and  now your
 {$age} years old";
 ?>
